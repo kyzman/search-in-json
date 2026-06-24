37c646921a2d800f05193481873910abc86b5fb4
Кузьмин <akuzmin@monetka.org>
2026-06-24 09:02:31 +0500

    add multiple files processing by mask

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ extern crate flate2;
 
 use clap::Parser;
 use flate2::read::GzDecoder;
+use glob::glob;
 use serde_json;
 use serde_json::Value;
-use std::env::args;
 use std::fs::File;
 use std::io::{IsTerminal, Read, stdout};
+use std::path::PathBuf;
 use std::time::Instant;
 
 #[derive(Parser, Debug, Clone)]
@@ -26,39 +27,72 @@ struct Args {
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let start = Instant::now();
-    print!("Загрузка и распаковка JSON...");
-
+    let total_start = Instant::now();
     let args = Args::parse();
-    // let path = "archive_90.json.gz";
-    let path = args.file;
-    let tar_gz = File::open(path)?;
-    let mut tar = GzDecoder::new(tar_gz);
-    let mut buf = String::new();
-    tar.read_to_string(&mut buf)?;
-    let mut out: Value = serde_json::from_str(&buf)?;
-    clean_json_value(&mut out); // Очистка JSON для корректного отображения и поиска
-    // 2. Что мы ищем (работает и для кириллицы, и для латиницы)
-    // let search_query = "параметры"; // Или "ScriptRunner", или "display_value"
-    // let search_query = "epaklina@monetka.ru"; // Или "ScriptRunner", или "display_value"
-    let elapsed = start.elapsed();
-    println!("{:.5} сек.", elapsed.as_secs_f64());
-
-    let start = Instant::now();
-    println!("Результаты поиска для: \"{}\"\n", args.search_query);
-
-    // Проверяем, поддерживает ли окружение цвета (is_terminal вернет false при перенаправлении в файл)
     let mut use_colors = false;
+    // Проверяем, поддерживает ли окружение цвета (is_terminal вернет false при перенаправлении в файл)
     if !args.mono {
         use_colors = stdout().is_terminal();
     }
-    // 3. Запуск поиска от корня ("$")
-    search_in_json(&out, &args.search_query, "$", use_colors, args.verbose);
-    // println!("{}", serde_json::to_string_pretty(&out)?);
-    // let mut archive = Archive::new(tar);
-    // archive.unpack(".")?;
-    let elapsed = start.elapsed();
-    println!("--- Поиск занял {:.5} сек.", elapsed.as_secs_f64());
+    let mask = args.file;
+
+    //  Если вы захотите искать файлы *.json.gz не только в папке temp, но и во всех её подпапках, вам достаточно просто изменить строку на r"C:\temp\**\*.json.gz". Две звездочки ** включают глубокое сканирование.
+    let found_files = find_files_by_pattern(&mask);
+    if found_files.is_empty() {
+        println!("Файлы не найдены.");
+    } else {
+        println!("Найдено файлов: {}", found_files.len());
+        for path in found_files {
+            let start = Instant::now();
+            println!("Загрузка и распаковка {}", path.display());
+            let tar_gz = File::open(&path)?;
+            let mut tar = GzDecoder::new(tar_gz);
+            let mut buf = String::new();
+            tar.read_to_string(&mut buf)?;
+            let mut out: Value = serde_json::from_str(&buf)?;
+
+            clean_json_value(&mut out); // Очистка JSON для корректного отображения и поиска
+
+            let elapsed = start.elapsed();
+            println!("{:.5} сек.", elapsed.as_secs_f64());
+
+            let start = Instant::now();
+            println!("Результаты поиска для: \"{}\"\n", args.search_query);
+
+            // 3. Запуск поиска от корня ("$")
+            let mut filename: &str = "";
+            if let Some(filename_str) = path.file_name().and_then(|os_str| os_str.to_str()) {
+                filename = filename_str;
+            } else {
+                println!("Имя файла содержит некорректные UTF-8 символы!");
+            };
+            search_in_json(
+                &out,
+                &args.search_query,
+                "$",
+                use_colors,
+                args.verbose,
+                filename,
+            );
+            // println!("{}", serde_json::to_string_pretty(&out)?);
+            // let mut archive = Archive::new(tar);
+            // archive.unpack(".")?;
+            let elapsed = start.elapsed();
+            println!(
+                "--- Поиск в файле {filename} занял {:.5} сек.",
+                elapsed.as_secs_f64()
+            );
+            drop(buf);
+            drop(tar);
+        }
+    }
+
+    let total_elapsed = total_start.elapsed();
+    println!(
+        "--- ОБЩИЙ Поиск занял {:.3} сек.",
+        total_elapsed.as_secs_f64()
+    );
+
     Ok(())
 }
 
@@ -90,7 +124,14 @@ fn clean_json_value(value: &mut Value) {
     }
 }
 
-fn search_in_json(value: &Value, query: &str, current_path: &str, use_colors: bool, verbose: bool) {
+fn search_in_json(
+    value: &Value,
+    query: &str,
+    current_path: &str,
+    use_colors: bool,
+    verbose: bool,
+    filename: &str,
+) {
     if query.is_empty() {
         return;
     }
@@ -104,7 +145,7 @@ fn search_in_json(value: &Value, query: &str, current_path: &str, use_colors: bo
                 // А. Проверка КЛЮЧА
                 if key.to_lowercase().contains(&query_lowercase) {
                     let highlighted_key = highlight_match(key, &query_lowercase, use_colors);
-                    println!("[Найдено в КЛЮЧЕ]");
+                    println!("[Найдено в КЛЮЧЕ файла {}]", filename);
                     // Подсвечиваем совпадение в самом пути
                     println!("Путь: {}.{}", current_path, highlighted_key);
                     if verbose {
@@ -112,20 +153,20 @@ fn search_in_json(value: &Value, query: &str, current_path: &str, use_colors: bo
                     };
                 }
 
-                search_in_json(val, query, &next_path, use_colors, verbose);
+                search_in_json(val, query, &next_path, use_colors, verbose, filename);
             }
         }
         Value::Array(arr) => {
             for (index, item) in arr.iter().enumerate() {
                 let next_path = format!("{}[{}]", current_path, index);
-                search_in_json(item, query, &next_path, use_colors, verbose);
+                search_in_json(item, query, &next_path, use_colors, verbose, filename);
             }
         }
         Value::String(s) => {
             // Б. Проверка ЗНАЧЕНИЯ
             if s.to_lowercase().contains(&query_lowercase) {
                 let highlighted_text = highlight_match(s, &query_lowercase, use_colors);
-                println!("[Найдено в ЗНАЧЕНИИ]");
+                println!("[Найдено в ЗНАЧЕНИИ файла {}]", filename);
                 println!("Путь: {}", current_path);
                 if verbose {
                     println!("Текст: \"{}\"\n", highlighted_text);
@@ -174,4 +215,32 @@ fn truncate_value(val: &Value) -> String {
     } else {
         s
     }
+}
+
+fn find_files_by_pattern(pattern: &str) -> Vec<PathBuf> {
+    let mut found_files = Vec::new();
+
+    // Функция glob сама разберет "C:\temp\*.json.gz" на каталог и маску.
+    // Она также поддерживает рекурсивный поиск, если указать "**/*.json.gz".
+    match glob(pattern) {
+        Ok(paths) => {
+            for entry in paths {
+                match entry {
+                    Ok(path) => {
+                        // Проверяем, что это файл, а не папка
+                        if path.is_file() {
+                            found_files.push(path);
+                        }
+                    }
+                    // Игнорируем ошибки чтения конкретных файлов (например, проблемы с правами доступа)
+                    Err(_) => continue,
+                }
+            }
+        }
+        Err(e) => {
+            println!("Ошибка: Некорректный синтаксис шаблона поиска: {}", e);
+        }
+    }
+
+    found_files
 }
